556342aa23b0c1feffdc17516723c578168792d3
Henry Cuffy <hcuffy_5@hotmail.com>
2018-07-28 17:33:42 +0200

    Add request call

--- controllers/stocks.js
+++ controllers/stocks.js
@@ -1,3 +1,18 @@
+//const axios = require('axios')
+const Stock = require('../models/stock')
+
 exports.getStock = (req, res, next) => {
-	
+	let userIP = req.connection.remoteAddress.replace(/^.*:/, '')
+	const { ticker , like } = req.body
+
+	// let stock = 'FB'
+	// axios({
+	// 	method:'get',
+	// 	url: 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol='+ stock + '&interval=1min&apikey='+ process.env.API_KEY,
+	// 	responseType:'json'
+	// })
+	// 	.then(function(response) {
+	//     	res.send(response.data)
+	// 	})
+
 }
