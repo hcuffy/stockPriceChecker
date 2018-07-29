6fae7e99e6258b74c1efb08a1ae878e41549a7c1
Henry Cuffy <hcuffy_5@hotmail.com>
2018-07-29 14:19:05 +0200

    Add double stock route

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const stockController = require('../controllers/stocks')
 
 router.get('/stock-price/', stockController.getStock)
+router.get('/stock-prices/', stockController.getTwoStocks)
 router.get('/', (req, res) => {
 	res.render('index')
 })
