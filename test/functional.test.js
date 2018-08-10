73057b1c3dbc5d933eb31540e96bd04e7b2de539
Henry Cuffy <hcuffy_5@hotmail.com>
2018-08-10 07:54:50 +0200

    Add test

--- test/functional.test.js
+++ test/functional.test.js
@@ -8,18 +8,36 @@ const { JSDOM } = jsdom
 chai.use(chaiHttp)
 
 describe('functional tests',  () => {
-	it('should get the stock price', function (done) {
+// it('should get the stock price', function (done) {
+// 	this.timeout(4000)
+// 	chai.request(server)
+// 		.get('/stock-price/')
+// 		.set('content-type', 'application/x-www-form-urlencoded')
+// 		.query({ ticker : 'MSFT' })
+// 		.end((err, res) => {
+// 			const dom = new JSDOM(res.text)
+// 			let output = dom.window.document.body.querySelector('.right-side').textContent
+// 			assert.equal(res.status, 200)
+// 			assert.isAtLeast(output.length, 6, 'output is greater or equal to 6')
+// 			done()
+// 		})
+// })
+
+	it('add stock with a like', function (done) {
 		this.timeout(4000)
 		chai.request(server)
 			.get('/stock-price/')
 			.set('content-type', 'application/x-www-form-urlencoded')
-			.query({ ticker : 'MSFT' })
+			.query({ ticker : 'MSFT' , like : 'on' })
 			.end((err, res) => {
 				const dom = new JSDOM(res.text)
-				let output = dom.window.document.body.querySelector('.right-side').textContent
+				let output = dom.window.document.body.querySelector('#likesId').textContent.slice(-1)
 				assert.equal(res.status, 200)
-				assert.isAtLeast(output.length, 6, 'output is greater or equal to 6')
+				assert.equal(output, 1)
 				done()
 			})
 	})
+
+
+
 })
